fea75ea4557f51a28318627943298a7fa072d3dd
Glautter Glauber Ferreira Neves <provider.gglauber@terceiros.mongeralaegon.com.br>
2019-11-21 23:34:50 -0300

    Arquivos de teste

---
+++ ExpressaoCalc.Test/CalculoExpressaoTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using ExpressaoCalc.App;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpressaoCalc.Test
+{
+    [TestClass]
+    public class CalculoExpressaoTest
+    {
+        private readonly string ExpressaoSoComParenteses = "3 * (2 + 5)";
+        private readonly string ExpressaoSoComParentesesSemEspaco = "3*(2+5)";
+        private ExpressaoNumerica ExpressaoMatematica { get; set; }
+        
+
+        [TestMethod]
+        public void DeveObterParenteseAberto()
+        {
+            ExpressaoMatematica = new ExpressaoNumerica(ExpressaoSoComParenteses);
+            ExpressaoMatematica.Parentese.AdicionarExpressao(ExpressaoSoComParenteses);
+            Assert.IsTrue(ExpressaoMatematica.Parentese.SinalAberto == "(");
+        }
+
+        [TestMethod]
+        public void DeveObterParenteseFechado()
+        {
+            ExpressaoMatematica = new ExpressaoNumerica(ExpressaoSoComParenteses);
+            ExpressaoMatematica.Parentese.AdicionarExpressao(ExpressaoSoComParenteses);
+            Assert.IsTrue(ExpressaoMatematica.Parentese.SinalFechado == ")");
+        }
+
+        [TestMethod]
+        public void DeveIdentificarSeTemSinal()
+        {
+            ExpressaoMatematica = new ExpressaoNumerica(ExpressaoSoComParenteses);
+            ExpressaoMatematica.Parentese.AdicionarExpressao(ExpressaoSoComParenteses);
+            Assert.IsTrue(ExpressaoMatematica.Parentese.TemSinal);
+        }
+
+        [TestMethod]
+        public void DeveRetirarOsEspacos()
+        {
+            ExpressaoMatematica = new ExpressaoNumerica(ExpressaoSoComParenteses);
+            Assert.IsTrue(ExpressaoMatematica.RetirarEspacos(ExpressaoSoComParenteses) == ExpressaoSoComParentesesSemEspaco);
+        }
+
+        [TestMethod]
+        public void DeveResolverAExpressao()
+        {
+            ExpressaoMatematica = new ExpressaoNumerica(ExpressaoSoComParenteses);
+            Assert.IsFalse(ExpressaoMatematica.Parentese.Resolver(ExpressaoSoComParenteses).Equals(ExpressaoSoComParenteses));
+        }
+    }
+}
---
+++ ExpressaoCalc.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExpressaoCalc.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExpressaoCalc.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd024aed-9692-46fd-8763-e242cf02e91f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
